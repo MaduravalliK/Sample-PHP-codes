14c999794431b22898bc8c4a8823d45032d59dc8
MaduravalliK <maduravalli82@gmail.com>
2018-12-06 13:34:30 +0530

    Simple PHP Jquery Ajax CRUD
    Simple PHP Jquery Ajax CRUD

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_USER', "root");
+	define('DB_PASSWORD', "");
+	define('DB_DATABASE', "products");
+	define('DB_HOST', "localhost");
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
